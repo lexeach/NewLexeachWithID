d9a22f94295a33bc338ba28950cd1d778d628dd6
lexeach <80155340+lexeach@users.noreply.github.com>
2024-02-28 14:41:46 +0530

    Update home.jsx

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -687,8 +687,8 @@ const Dashboard = () => {
             <div className="card">
               <div className="card-body">
                 <h6>Income</h6>
-                <h4 className="mb-0">{udIncome ? udIncome.toFixed(4) : 0} USDT
-                </h4>
+                <h4 className="mb-0">{udIncome ? udIncome : 0} USDT
+                 </h4>
               </div>
             </div>
           </div>
